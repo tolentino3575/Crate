4419044f102a67b2ecb20d47925d7b54a8ba1077
Jeff Seymour, Lauryn Davis and Molly Curtin <seym1585@gmail.com>
2016-03-08 15:58:23 -0800

    add media queries for medium and small devices

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   // toggles login form on sidebar
   $('.login-toggle').click(function() {
     $('.login').slideToggle('slow');
+    $('.search').slideToggle('slow');
   })
 
 });
