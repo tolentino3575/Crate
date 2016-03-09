65f5f6e9da5e57f0fea612182d757501646e6135
Edward Duro <e.jamesduro@gmail.com>
2016-03-08 16:27:34 -0800

    fix conflicts

--- app/app.php
+++ app/app.php
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-    <?php
+
+   <?php
 
 
     require_once __DIR__.'/../vendor/autoload.php';
@@ -186,28 +186,4 @@
     });
 
     return $app;
-=======
-<?php
-	require_once __DIR__.'/../vendor/autoload.php';
-
-	$app = new Silex\Application();
-
-	$server = 'mysql:host=localhost;dbname=discogs';
-    $username = 'root';
-    $password = 'root';
-    $DB = new PDO($server, $username, $password);
-
-	$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
-
-	//$app['debug'] = TRUE;
-
-	use Symfony\Component\HttpFoundation\Request;
-    Request::enableHttpMethodParameterOverride();
-
-  $app->get("/", function() use($app){
-    return $app['twig']->render('index.html.twig');
-  });
-  return $app;
-
->>>>>>> master
 ?>
