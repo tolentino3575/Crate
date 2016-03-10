b293f95c97586136fc986218295a9836e98e39dc
eddieduro <e.jamesduro@gmail.com>
2016-03-09 16:49:08 -0800

    fix merge conflicts

--- app/app.php
+++ app/app.php
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 
-   <?php
-   session_start();
-=======
  <?php
     session_start();
->>>>>>> master
+
 
     require_once __DIR__.'/../vendor/autoload.php';
     require_once __DIR__.'/../src/User.php';
