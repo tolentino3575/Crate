1fbaa6deb8898d3ab5b04311eac0a0bfb7ed597f
Jeff Seymour <seym1585@gmail.com>
2016-03-10 16:13:13 -0800

    add create account form on sidebar and refactor button code

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
   // toggles login form on sidebar
   $('.login-toggle').click(function() {
+    $('.account-form').slideUp();
     $('.login').slideToggle('slow');
     $('.search').slideToggle('slow');
+    $('.login-form').show('slow');
+    $('.create-account').html("Create An Account");
+  })
+
+  // toggles create account form on sidebar
+  $('.create-account').click(function() {
+    $('.create-account').html("New Account Info:");
+    $('.login-form').slideUp('slow');
+    $('.account-form').slideDown('slow');
   })
 });
